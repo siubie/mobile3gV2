e1c5e05e994d24f0229f05a81efc1560030c1e92
Putra Prima A <siubie@users.noreply.github.com>
2021-09-14 10:50:25 +0700

    Initial commit

---
+++ .github/test/test.js
@@ -0,0 +1,26 @@
+const glob = require("glob");
+const { countCharacters } = require("wcmd/lib/utils");
+const MINIMAL_CHARS = 1000;
+
+(async () => {
+  glob("../../attendances/**/*.md", (err, files) => {
+    files.forEach((file) => {
+      try {
+        const result = countCharacters(file);
+        const chars = result.lines
+          .map((line) => line.length)
+          .reduce((l1, l2) => l1 + l2);
+        if (chars < MINIMAL_CHARS) {
+          console.error(
+            `${file} doesn't meet summary requirements (minimum ${MINIMAL_CHARS} chars): ${chars} chars`
+          );
+        }
+      } catch (error) {
+        console.error(`${file} contains unsupported character format`);
+      }
+      process.exit(1);
+    });
+    console.log(`All summary passed`);
+    process.exit(0);
+  });
+})();
